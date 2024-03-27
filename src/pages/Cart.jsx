caf218650606d4812d49a924004a4cf985da65ed
charletton <emitalque@gmail.com>
2024-03-27 14:12:05 -0300

    cart empty add

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,42 +1,44 @@
+import { useEffect, useContext, useState } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { CartEmpty } from "../pages/CartEmpty";
 import NavBar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
 import { useTheme } from "../contexts/ThemeContext";
-import { CartContext } from "../contexts/CartContext";
-import { useContext, useState } from "react";
 
 const Cart = () => {
-  const { cart, clear } = useContext(CartContext); 
-  const [cartQuantity, setCartQuantity] = useState(0); // Usa un estado local para la cantidad del carrito
-  console.log(cart)
-  
-  const { theme ,setTheme } = useTheme();
+  const { cart, clear } = useContext(CartContext);
+  const [cartQuantity, setCartQuantity] = useState(0);
+  const { theme, setTheme } = useTheme();
 
-return (
-  <div className={`app ${theme}`}>
-    <NavBar theme={theme} setTheme={setTheme} /> 
-    <h1 className="text-4xl mt-10 font-bold text-center mt-8 mb-12 text-red-600">Carrito</h1>
-    <button onClick={clear} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Limpiar Carrito</button>
-    <hr />
-    {/* main body */}
-    <div className={`Main-body p-4 w-full h-full ${theme === 'dark' ? 'bg-dark' : 'bg-white'}`}>
-      {cart.map((item, index) => {
-        console.log('El item es:');
-        console.log(item);
-        return (
+  // Actualiza cartQuantity cuando cambia el carrito
+  useEffect(() => {
+    setCartQuantity(cart.length);
+  }, [cart]);
+
+  if (cartQuantity === 0) {
+    return <CartEmpty />;
+  }
+
+  return (
+    <div className={`app ${theme}`}>
+      <NavBar theme={theme} setTheme={setTheme} />
+      <h1 className="text-4xl mt-10 font-bold text-center mt-8 mb-12 text-red-600">Carrito</h1>
+      <button onClick={clear} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">Limpiar Carrito</button>
+      <hr />
+      <div className={`Main-body p-4 w-full h-full ${theme === 'dark' ? 'bg-dark' : 'bg-white'}`}>
+        {cart.map((item, index) => (
           <div key={index} className={`cart-item ${theme === 'dark' ? 'text-white' : 'text-dark'}`}>
-            <h2>{item.item.nombre}</h2> {/* Accede al nombre del artículo correctamente */}
-            <p>Cantidad: {item.quantity}</p> 
-            <p>Precio unidad: ${item.item.price }</p> {/* Accede al precio del artículo correctamente */}
-            <p>Precio total: ${item.item.price * item.quantity}</p> {/* Accede al precio del artículo correctamente */}
+            <h2>{item.item.nombre}</h2>
+            <p>Cantidad: {item.quantity}</p>
+            <p>Precio unidad: ${item.item.price}</p>
+            <p>Precio total: ${item.item.price * item.quantity}</p>
             <br />
           </div>
-        );
-      })}
+        ))}
+      </div>
+      <Footer />
     </div>
-    <Footer/>
-  </div>
-)};
+  );
+};
 
-
- 
 export default Cart;
--- src/pages/CartEmpty.jsx
+++ src/pages/CartEmpty.jsx
@@ -1,7 +1,27 @@
-const CartEmpty = () => {
-    return ( 
-        <h1>Carrito vacio!</h1>
-     );
-}
- 
-export default CartEmpty;
+import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer/Footer';
+import { useTheme } from '../contexts/ThemeContext';
+import { Link } from 'react-router-dom';
+
+export const CartEmpty = () => {
+  const { theme, setTheme } = useTheme();
+
+  return ( 
+    <div className={`app ${theme}`}>
+      <NavBar theme={theme} setTheme={setTheme} /> 
+
+      {/* main body */}
+      <div className={` h-screen m-0 p-0 flex flex-col items-center justify-center ${theme === 'dark' ? 'text-dark' : 'text-white'}`}>
+        <h1 className={`text-4xl md:text-6xl mt-8 font-bold mb-8 ${theme == 'dark' ? 'text-white' : 'text-dark'}`}>¡Carrito vacío!</h1>
+        <img src='https://w7.pngwing.com/pngs/270/822/png-transparent-shopping-cart-purchase-market-trolley-shopping-cart-red.png' alt="Carrito vacío" className="w-64 md:w-96 mt-5" />
+        <p className={`text-lg md:text-xl mt-8 ${theme == 'dark' ? 'text-white' : 'text-dark'}`}>Tu carrito de compras está vacío. ¡Ve a explorar nuestros productos y encuentra algo que te guste!</p>
+        <Link to="/productos">
+          <button className="bg-blue-500 mt-5 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Explorar productos
+          </button>
+        </Link>
+      </div>
+      <Footer theme={theme} setTheme={setTheme}/> 
+    </div>
+  );
+};
